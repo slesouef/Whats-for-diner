fcb288207c95eadedb16a4c9ad64460146adff5b
slesouef <sebastienlesouef@protonmail.com>
2021-04-11 20:39:37 +0200

    simplify non-prod static conf

--- recipe_search/settings.py
+++ recipe_search/settings.py
@@ -145,6 +145,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if os.environ.get('ENV') == 'PROD':
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
